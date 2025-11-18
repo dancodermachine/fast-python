be5ceb150e00dcdfdb881e88f9154e77f9b65d0f
Daniel <60199577+dancodermachine@users.noreply.github.com>
2025-11-18 21:26:44 +0000

    06_processpoolexecutor chap3, chap4, chap5, and middle of chap6

--- run_code.py
+++ run_code.py
@@ -3,22 +3,22 @@ from time import sleep
 from multiprocessing import Process
 from concurrent.futures import ProcessPoolExecutor, TimeoutError, wait, as_completed, FIRST_COMPLETED, FIRST_EXCEPTION 
 
-# custom function to be executed in a worker process
+# custom function executed by a worker process
 def task(number):
-    # report a message
-    print(f"Worker task {number}...", flush=True)
-    # block for a moment
-    sleep(1)
-    
-# initialize a worker in a process pool
-def init():
-    # report a message
-    print("Initializing worker ...", flush=True)
-    
+    # block for a fraction of a second
+    sleep(random())
+    # return task number
+    return number
+
 # protect the entry point
 if __name__ == "__main__":
-    # create and configure the process pool
-    with ProcessPoolExecutor(2, initializer=init) as exe:
-        # issue tasks to the process pool
-        _ = exe.map(task, range(4))
-    
+    # start the process pool
+    with ProcessPoolExecutor(10) as exe:
+        # submit tasks and collect futures 
+        futs = [exe.submit(task, i) for i in range(10)]
+        # handle task results as they are completed
+        for future in as_completed(futs):
+            # retrieve the result
+            result = future.result()
+            # report the result
+            print(f"> result for task {result}")
